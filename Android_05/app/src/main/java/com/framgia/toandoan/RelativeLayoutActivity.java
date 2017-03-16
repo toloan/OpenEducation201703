e0d7d5152369d912de9d2f1983cef06c3f57175c
ToanDoan <toandoan.coder@gmail.c>
2017-03-16 20:14:35 +0700

    COntainer_View

--- Android_05/app/src/main/java/com/framgia/toandoan/RelativeLayoutActivity.java
+++ Android_05/app/src/main/java/com/framgia/toandoan/RelativeLayoutActivity.java
@@ -1,12 +1,12 @@
 package com.framgia.toandoan;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RelativeLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relative_layout);
     }
 }
